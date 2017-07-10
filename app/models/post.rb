51ca7cc15cb47498f417ce50bd6147fc19904993
shivi <srpurwaha201@gmail.com>
2017-07-11 01:25:04 +0530

    add time-info of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,24 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  def timediff
+    diff = (Time.now - self.updated_at).round()
+    type = nil;
+    if diff < 60
+      type = "seconds"
+    elsif diff < 3600
+      diff = diff/60
+      type = "minutes"
+    elsif diff < 86400
+      type = "hours"
+      diff = diff/3600
+    else
+      type = "days"
+      diff = diff/86400
+    end
+    time = {}
+    time[:type] = type
+    time[:diff] = diff
+    return time
+  end
 end
