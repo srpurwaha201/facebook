bab48a58da0fc60be3d16e90aed997143f25f745
shivi <srpurwaha201@gmail.com>
2017-07-19 20:15:52 +0530

    add delete button to comment and post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,14 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to '/', notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
