e248e14c56f3853ec8cacdf04d9d9e6ffa1e887a
shivi <srpurwaha201@gmail.com>
2017-07-20 22:06:53 +0530

    delete comment via ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -55,9 +55,11 @@ class CommentsController < ApplicationController
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    @comment_id = @comment.id
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to '/', notice: 'Comment was successfully destroyed.' }
+      format.js{  }
+      # format.html { redirect_to '/', notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
