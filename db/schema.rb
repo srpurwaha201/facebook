5d3bd6475fe4d035a7a23bf5a80ad64074dd8e89
shivi <srpurwaha201@gmail.com>
2017-07-24 18:58:50 +0530

    add birthday to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719164103) do
+ActiveRecord::Schema.define(version: 20170724125821) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170719164103) do
     t.string   "profile_picture"
     t.string   "first_name"
     t.string   "last_name"
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
